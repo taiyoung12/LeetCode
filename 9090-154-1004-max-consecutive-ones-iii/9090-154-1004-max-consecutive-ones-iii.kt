7a38eec731156b540420301644f186d34eb0bc97
taiyoung <35217809+taiyoung12@users.noreply.github.com>
2025-01-26 19:24:33 +0900

    Time: 1164 ms, Memory: 41.2 MB - LeetHub

---
+++ 9090-154-1004-max-consecutive-ones-iii/9090-154-1004-max-consecutive-ones-iii.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun longestOnes(nums: IntArray, k: Int): Int {
+        var start = 0
+        var end = 0 
+        var zeros = 0 
+
+        while(end < nums.size){
+            println("$start , $end")
+            if(nums[end] == 0){
+                zeros++
+            }
+            end++
+
+            if(zeros > k){
+                if(nums[start] == 0){
+                    zeros--
+                }
+                start++
+            }
+        }
+
+        return end - start 
+    }
+}
